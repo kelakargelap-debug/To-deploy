cb9bf8cf9e96a39c881adc3a3cbdfca7d0e5cda9
kelakargelap-debug <kelakargelap@gmail.com>
2026-06-09 16:40:54 +0900

    Fix Render 500 error: SQLite permissions and APP_KEY

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -14,5 +14,8 @@ if [ "$APP_SEED" = "true" ]; then
     php artisan db:seed --force
 fi
 
+# Fix permissions for SQLite database and storage so www-data can access them
+chown -R www-data:www-data /var/www/html/database /var/www/html/storage /var/www/html/bootstrap/cache
+
 # Start supervisord to run Nginx and PHP-FPM
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
